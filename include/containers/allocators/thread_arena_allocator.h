f96164380db3694537fb25b8d98506e45b2eab8c
Roman Paukner <romanpauk@gmail.com>
2025-02-22 23:43:38 +0100

    thread_arena_allocator min alignment

--- include/containers/allocators/thread_arena_allocator.h
+++ include/containers/allocators/thread_arena_allocator.h
@@ -32,18 +32,24 @@ struct thread_mmap_arena_factory {
     }
 };
 
-template <typename T, typename ArenaFactory = thread_arena_factory< 1<<16 > > class thread_arena_allocator {
+template <typename T, typename ArenaFactory = thread_arena_factory< 1<<16 >, std::size_t MinAlignment = alignof(std::max_align_t) > class thread_arena_allocator {
+    static_assert((MinAlignment & (MinAlignment - 1)) == 0);
 public:
     using value_type    = T;
     using resource_mark_type = typename ArenaFactory::resource_mark_type;
+    static constexpr std::size_t alignment = std::max(MinAlignment, arena_allocator_alignment_v<T>);
+
+    template < typename U > struct rebind {
+        using other = thread_arena_allocator< U, ArenaFactory, MinAlignment >;
+    };
 
     thread_arena_allocator() = default;
-    template <typename U> thread_arena_allocator(const thread_arena_allocator<U, ArenaFactory>&) noexcept {}
+    template <typename U, std::size_t AlignmentU> thread_arena_allocator(const thread_arena_allocator<U, ArenaFactory, AlignmentU>&) noexcept {}
 
     value_type* allocate(std::size_t n) {
         if (std::numeric_limits<intptr_t>::max() / sizeof(T) < n)
             return nullptr;
-        return reinterpret_cast<value_type*>(ArenaFactory::get()->allocate(sizeof(T) * n, alignof(T)));
+        return reinterpret_cast<value_type*>(ArenaFactory::get()->allocate(sizeof(T) * n, alignment));
     }
 
     void deallocate(value_type* ptr, std::size_t n) noexcept {
@@ -53,13 +59,28 @@ public:
     static resource_mark_type resource_mark() { return ArenaFactory::get(); }
 };
 
-template <typename T, typename U, typename ArenaFactory>
-bool operator == (const thread_arena_allocator<T, ArenaFactory>&, const thread_arena_allocator<U, ArenaFactory>&) noexcept {
+template <typename ArenaFactory, std::size_t MinAlignment> class thread_arena_allocator<void, ArenaFactory, MinAlignment> {
+public:
+    using resource_mark_type = typename ArenaFactory::resource_mark_type;
+
+    template < typename U > struct rebind {
+        using other = thread_arena_allocator< U, ArenaFactory, MinAlignment >;
+    };
+
+    thread_arena_allocator() = default;
+    template <typename U, std::size_t AlignmentU> thread_arena_allocator(const thread_arena_allocator<U, ArenaFactory, AlignmentU>&) noexcept {}
+
+    static resource_mark_type resource_mark() { return ArenaFactory::get(); }
+};
+
+
+template <typename T, std::size_t AlignmentT, typename U, std::size_t AlignmentU, typename ArenaFactory>
+bool operator == (const thread_arena_allocator<T, ArenaFactory, AlignmentT>&, const thread_arena_allocator<U, ArenaFactory, AlignmentU>&) noexcept {
     return true;
 }
 
-template <typename T, typename U, typename ArenaFactory>
-bool operator != (const thread_arena_allocator<T, ArenaFactory>& x, const thread_arena_allocator<U, ArenaFactory>& y) noexcept {
+template <typename T, std::size_t AlignmentT, typename U, std::size_t AlignmentU, typename ArenaFactory>
+bool operator != (const thread_arena_allocator<T, ArenaFactory, AlignmentT>& x, const thread_arena_allocator<U, ArenaFactory, AlignmentU>& y) noexcept {
     return !(x == y);
 }
 
