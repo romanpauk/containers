f57bd37bb139db4151a29128644f907db135e532
Roman Paukner <romanpauk@gmail.com>
2025-08-06 03:49:20 +0200

    new pool

--- include/containers/allocators/pool.h
+++ include/containers/allocators/pool.h
@@ -130,58 +130,262 @@ namespace containers {
         std::vector<T> values_;
     };
 
+    struct memory_buffer_builder {
+        memory_buffer_builder() = default;
+
+        template<typename T, std::size_t Alignment = alignof(T)> void add() {
+            size_ += sizeof(T) + Alignment - 1;
+        }
+
+        std::size_t size() const { return size_; }
+    private:
+        std::size_t size_ = 0;
+    };
+
+    struct memory_buffer_allocator {
+        memory_buffer_allocator(void* buffer, std::size_t size)
+            : begin_((uintptr_t)buffer)
+            , current_(begin_)
+            , size_(size)
+        {}
+
+        template<typename T, std::size_t Alignment = alignof(T)> T* allocate() {
+            current_ = (current_ + Alignment - 1) & ~(Alignment - 1);
+            if (current_ + sizeof(T) > begin_ + size_)
+                std::abort();
+
+            T* ptr = (T*)current_;
+            current_ += sizeof(T);
+            return ptr;
+        }
+
+    private:
+        uintptr_t begin_;
+        uintptr_t current_;
+        std::size_t size_;
+    };
+
+    template< std::size_t Size > struct PageSizeClass {
+        static_assert((Size & (Size - 1)) == 0);
+        static_assert(Size <= 65536 && Size >= 1024);
+        static constexpr std::size_t Index = Size >> 11;
+    };
+
+    template< std::size_t Size > struct PageManager {
+        static constexpr std::size_t PageSize = 1 << 22;
+        static constexpr std::size_t PageCount = Size / PageSize;
+        static constexpr std::size_t ChunkSize = PageSize / 64;
+
+        struct PageMetadata {
+            bitmap<64> pagebits; // TODO: needed?
+            std::array<bitmap<64>, 7> sizebits;
+            std::array<bitmap<64>, 64> chunkbits;
+        };
+
+        void *memory_;
+        std::size_t memory_size_;
+
+        using MetaSpace = std::array< PageMetadata, PageCount >;
+        MetaSpace* meta_space_;
+
+        using PageLiveset = std::array<bitmap<PageCount>, 7>;
+        PageLiveset* page_liveset_;
+
+        using PageDeadset = bitmap<PageCount>;
+        PageDeadset* page_deadset_;
+
+        using PageCache = std::array< void*, 7 >;
+        PageCache* page_cache_;
+
+        using PageSpace = std::array< std::array< std::array<uint8_t, ChunkSize>, 64>, PageCount >;
+        PageSpace* page_space_;
+
+        PageManager() {
+            memory_buffer_builder builder;
+            builder.add<MetaSpace>();
+            builder.add<PageLiveset>();
+            builder.add<PageDeadset>();
+            builder.add<PageCache>();
+            builder.add<PageSpace, PageSize>();
+
+            memory_size_ = builder.size();
+            memory_ = mmap(0, memory_size_, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+            memory_buffer_allocator allocator(memory_, memory_size_);
+            meta_space_ = allocator.allocate<MetaSpace>();
+            page_liveset_ = allocator.allocate<PageLiveset>();
+            page_deadset_ = allocator.allocate<PageDeadset>();
+            page_cache_ = allocator.allocate<PageCache>();
+            page_space_ = allocator.allocate<PageSpace, PageSize>();
+        }
+
+        ~PageManager() {
+            munmap(memory_, memory_size_);
+        }
+
+        template< typename SizeClass > void* allocate() {
+        again:
+            auto page = get_live_page<SizeClass>();
+            if (__unlikely__(page == -1))
+                std::abort();
+            auto chunk = allocate_chunk<SizeClass>(page);
+            if (__unlikely__(chunk == -1)) {
+                goto again;
+            }
+            return get_address(page, chunk);
+        }
+
+        template< typename SizeClass > void get_live_page() {
+            auto& page = (*page_cache_)[SizeClass::Index];
+            if (__unlikely__(page == -1)) {
+                //
+                // TODO: check live-set
+                //
+                // Live-set unusable, allocate fresh page
+                page = allocate_page();
+            }
+            return page;
+        }
+
+        std::size_t allocate_page() {
+            // check dead-set
+            // if empty, allocate new page
+        }
+
+        template< typename SizeClass > std::size_t allocate_chunk(std::size_t page) {
+            auto& metadata = (*meta_space_)[page];
+        }
+
+        void* get_address(std::size_t page_index, std::size_t chunk_index) {
+            return (*page_space_)[page_index][chunk_index];
+        }
+
+        std::size_t get_page_index(void* ptr) {
+            auto page = ((uintptr_t)page - (uintptr_t)page_space_) / PageSize;
+            assert(page < page_space->size());
+            return page;
+        }
+
+        std::size_t get_chunk_index(void* ptr) {
+        }
+
+        template< typename SizeClass > void mark_live(void* ptr, bool) {
+            auto page = get_page_index(ptr);
+            // TODO: assert
+            (*page_liveset_)[SizeClass::Index].set_bit(page);
+        }
+
+        void mark_dead(void* ptr, bool) {
+            auto page = get_page_index(ptr);
+            // TODO: assert
+            (*page_deadset_)[SizeClass::Index].set_bit(page);
+        }
+
+        void deallocate(void* ptr) {
+            auto page = get_page_index(ptr);
+            auto chunk = get_chunk_index(ptr);
+            (*meta_space_)[page];
+            // Deallocate chunk
+            //
+            // If page is not cached and it is empty, deallocate it
+            //  (write to deadset)
+        }
+    };
+
     template<std::size_t Size, std::size_t Count> struct page_manager {
         static constexpr std::size_t PageSize = Size;
         static_assert((PageSize & (PageSize - 1)) == 0);
+        static_assert(PageSize == 65536);
         static constexpr std::size_t PageCount = Count;
 
         page_manager() {
-            mmap_base_ = (uint64_t)mmap(0, PageSize * PageCount, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-            mmap_current_ = mmap_base_;
+            mmap_base_ = (uint64_t)mmap(0, PageSize * (PageCount + 1), PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+            mmap_current_ = mmap_start_ = (mmap_base_ + PageSize - 1) & ~(PageSize - 1);
         }
 
         ~page_manager() {
-            munmap((void*)mmap_base_, PageSize * PageCount);
+            munmap((void*)mmap_base_, PageSize * (PageCount + 1));
         }
 
-        void* allocate() {
+        template< std::size_t SizeClass > void* allocate() {
+            assert((SizeClass & (SizeClass - 1)) == 0);
+
+            void*& page = page_class_[SizeClass >> 11];
+            if (__unlikely__(!page)) {
+            again:
+                page = allocate_page<SizeClass>();
+                if (!page)
+                    std::abort();
+            }
+
+            auto& metadata = page_metadata_[get_page_index(page)];
+            auto index = metadata.tzcnt();
+            if (index < metadata.size()) {
+                return (void*)((uintptr_t)page + SizeClass * index);
+            } else {
+                goto again;
+            }
+        }
+
+        template< std::size_t SizeClass > void* allocate_page() {
             void* ptr = nullptr;
-            auto index = deallocated_pages_.tzcnt();
-            if (index < deallocated_pages_.size()) {
-                deallocated_pages_.clear_bit(index);
+            // TODO: this needs to be per SizeClass
+            auto& liveset = page_class_live_[SizeClass >> 11];
+            auto index = liveset.tzcnt();
+            if (index < liveset.size()) {
+                liveset.clear_bit(index);
                 ptr = get_page(index);
             } else {
                 ptr = (void*)mmap_current_;
                 mmap_current_ += PageSize;
-                if (mmap_current_ > mmap_base_ + PageSize * PageCount)
+                if (mmap_current_ > mmap_start_ + PageSize * PageCount)
                     return nullptr;
             }
 
-            assert(deallocated_pages_.get_bit(get_page_index(ptr)) == 0);
+            assert(liveset.get_bit(get_page_index(ptr)) == 0);
+            page_metadata_[get_page_index(ptr)].set(0);
             return (void*)ptr;
         }
 
-        void deallocate(void* ptr) {
-            assert(deallocated_pages_.get_bit(get_page_index(ptr)) == 0);
-            deallocated_pages_.set_bit(get_page_index(ptr));
+        template< std::size_t SizeClass > void deallocate(void* ptr) {
+            auto& metadata = page_metadata_[get_page_index(ptr)];
+            assert(metadata.get_bit(get_page_class_index<SizeClass>(ptr)) == 1);
+            metadata.set_bit(get_page_class_index<SizeClass>(ptr));
+            if (metadata.get() == -1) {
+                page_class_live_[SizeClass >> 11].clear_bit(get_page_index(ptr));
+                deallocate_page(ptr);
+            } else {
+                page_class_live_[SizeClass >> 11].set_bit(get_page_index(ptr));
+            }
         }
 
         uint64_t get_page_index(void* ptr) {
-            auto index = ((uint64_t)ptr - mmap_base_) / PageSize;
+            auto index = ((uint64_t)ptr - mmap_start_) / PageSize;
             assert(index < PageCount);
             return index;
         }
 
-        void* get_page(uint64_t index) {
+        void* get_page_address(uint64_t index) {
             assert(index < PageCount);
-            return (void*)(mmap_base_ + PageSize * index);
+            return (void*)(mmap_start_ + PageSize * index);
         }
 
     private:
         uint64_t mmap_base_;
+        uint64_t mmap_start_;
         uint64_t mmap_current_;
 
         bitmap<PageCount> deallocated_pages_;
+
+        // page_manager will allow allocation in steps of
+        // 1024, 2048, 4096, 8192...
+        // TODO: this is fixed now for page size of 65536
+        bitmap<PageSize/1024> page_metadata_[PageCount] = {0};
+
+        static std::array<void*, 7> page_class_;
+
+        // Live pages available for allocation
+        static std::array< bitmap<PageCount>, 7 > page_class_live_;
     };
 
     template<typename T, typename PageManager> struct pool_page_allocator {
@@ -230,11 +434,10 @@ namespace containers {
                 : allocations(init) {}
 
             uint8_t memory[sizeof(T) * N];
-            bitmap<64> allocations;
+            bitmap<N> allocations;
         };
 
         static_assert(sizeof(page) <= Allocator::PageSize);
-        static_assert(bitmap<64>::size() <= N);
 
         pool_allocator(Allocator& allocator)
             : pool_page_allocator_(allocator) {}
@@ -271,6 +474,8 @@ namespace containers {
 goto again;
         }
 
+        // It would be nice to have no branch in deallocation path
+        // as it can't fail
         void deallocate(T* ptr) {
             page* p = get_page(ptr);
             assert(p->allocations.get_bit(get_element_index(ptr)) == 0);
